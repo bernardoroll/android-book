21bdc6af980e3f2f5ad4d61f739e45cb8eafc0b9
Bernardo Rocha Salgueiro <bernardo.roll@gmail.com>
2015-07-10 16:55:03 -0300

    Criação dos métodos na classe SGActivity
    Signed-off-by: Bernardo Rocha Salgueiro <bernardo.roll@gmail.com>

--- SimpleGameEngineV1/src/com/book/simplegameengine_v1/SGActivity.java
+++ SimpleGameEngineV1/src/com/book/simplegameengine_v1/SGActivity.java
@@ -1,7 +1,53 @@
 package com.book.simplegameengine_v1;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
 public class SGActivity extends Activity {
+	
+	public static final String TAG = "SimpleGameEngine";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		Log.v(TAG, "onCreate() chmado.");
+	}
+	
+	@Override
+	protected void onStart(){
+		super.onStart();
+		Log.v(TAG, "onStart() chamado.");
+	}
+	
+	@Override
+	protected void onResume(){
+		super.onResume();
+		Log.v(TAG, "onResume() chamado.");
+	}
+	
+	@Override
+	protected void onRestart(){
+		super.onRestart();
+		Log.v(TAG, "onRestart() chamado.");
+	}
+	
+	@Override
+	protected void onPause(){
+		super.onPause();
+		Log.v(TAG, "onPause() chamado.");
+	}
+	
+	@Override
+	protected void onStop(){
+		super.onStop();
+		Log.v(TAG, "onStop() chamado.");
+	}
+	
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+		Log.v(TAG, "onDestroy() chamado.");
+	}
 
 }
